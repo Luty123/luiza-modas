dd92c14ca8eeb421a1e6eb90806a6759c357e89e
Luciano <lucianopaiva.e@gmail.com>
2021-02-13 20:53:17 -0300

    Alterações no drawer

--- lib/tiles/drawer_tile.dart
+++ lib/tiles/drawer_tile.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 class DrawerTile extends StatelessWidget {
   final IconData icon;
   final String text;
+  final PageController controller;
+  final int page;
 
-  DrawerTile(this.icon, this.text);
+  DrawerTile(this.icon, this.text, this.controller, this.page);
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +14,10 @@ class DrawerTile extends StatelessWidget {
       color: Colors.transparent,
 // Função para linkar as outras paginas
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.of(context).pop();
+          controller.jumpToPage(page);
+        },
 // Lista do menu - opções
         child: Container(
           height: 50.0,
@@ -21,14 +26,21 @@ class DrawerTile extends StatelessWidget {
               Icon(
                 icon,
                 size: 35.0,
-                color: Colors.black,
+                color: controller.page.round() == page
+                    ? Theme.of(context).primaryColor
+                    : Colors.grey[700],
               ),
               SizedBox(
                 width: 30.0,
               ),
               Text(
                 text,
-                style: TextStyle(fontSize: 20.0, color: Colors.black),
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: controller.page.round() == page
+                      ? Theme.of(context).primaryColor
+                      : Colors.grey[700],
+                ),
               )
             ],
           ),
--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:loja/tiles/drawer_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
+  final PageController pageController;
+  CustomDrawer(this.pageController);
   @override
 // Gradiente
   Widget build(BuildContext context) {
@@ -67,10 +69,11 @@ class CustomDrawer extends StatelessWidget {
                 ),
               ),
               Divider(),
-              DrawerTile(Icons.home, "Inicio"),
-              DrawerTile(Icons.list, "Produtos"),
-              DrawerTile(Icons.location_on, "Lojas"),
-              DrawerTile(Icons.playlist_add_check, "Meus Pedidos"),
+              DrawerTile(Icons.home, "Inicio", pageController, 0),
+              DrawerTile(Icons.list, "Produtos", pageController, 1),
+              DrawerTile(Icons.location_on, "Lojas", pageController, 2),
+              DrawerTile(
+                  Icons.playlist_add_check, "Meus Pedidos", pageController, 3),
             ],
           )
         ],
