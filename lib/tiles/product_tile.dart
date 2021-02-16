7869faf6003ac63403265b30caa27de0db01d3c1
Luciano <lucianopaiva.e@gmail.com>
2021-02-15 23:37:47 -0300

    Estrutura da lista de produtos, tabs - grid e list

---
+++ lib/tiles/product_tile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:loja/datas/product_data.dart';
+
+class ProductTile extends StatelessWidget {
+  final String type;
+  final ProductData data;
+
+  ProductTile(this.type, this.data);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
