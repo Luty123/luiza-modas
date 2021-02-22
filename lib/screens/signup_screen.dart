c473a5d1117e00ecca6a1e33d175c5ac406f4011
Luciano <lucianopaiva.e@gmail.com>
2021-02-21 22:52:41 -0300

    Estruturando cadastro e salvando dados no firebase

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:loja/models/user_model.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class SignUpScreen extends StatelessWidget {
+class SignUpScreen extends StatefulWidget {
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+  final _addressController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
@@ -11,71 +21,98 @@ class SignUpScreen extends StatelessWidget {
         title: Text("CRIAR CONTA"),
         centerTitle: true,
       ),
-      body: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.all(8.0),
-          children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(hintText: "Nome completo"),
-              validator: (text) {
-                if (text.isEmpty) return "Nome inválido!";
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: "E-mail"),
-              keyboardType: TextInputType.emailAddress,
-              validator: (text) {
-                if (text.isEmpty || !text.contains("@"))
-                  return "E-mail inválido!";
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: "Senha"),
-              obscureText: true,
-              validator: (text) {
-                if (text.isEmpty || text.length < 6)
-                  return "Senha inválida! A senha deve conter 6 digitos";
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: "Endereço"),
-              validator: (text) {
-                if (text.isEmpty) return "Endereço inválido!";
-              },
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            SizedBox(
-              height: 50.0,
-              child: ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {}
-                },
-                child: Text(
-                  "CRIAR CONTA",
-                  style: TextStyle(
-                    fontSize: 18.0,
-                  ),
+      body: ScopedModelDescendant<UserModel>(
+        builder: (context, child, model) {
+          if (model.isLoading)
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          return Form(
+            key: _formKey,
+            child: ListView(
+              padding: EdgeInsets.all(8.0),
+              children: <Widget>[
+                TextFormField(
+                  controller: _nameController,
+                  decoration: InputDecoration(hintText: "Nome completo"),
+                  validator: (text) {
+                    if (text.isEmpty) return "Nome inválido!";
+                  },
                 ),
-                style: TextButton.styleFrom(
-                    primary: Colors.white,
-                    backgroundColor: Theme.of(context).primaryColor),
-              ),
-            )
-          ],
-        ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(hintText: "E-mail"),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (text) {
+                    if (text.isEmpty || !text.contains("@"))
+                      return "E-mail inválido!";
+                  },
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextFormField(
+                  controller: _passController,
+                  decoration: InputDecoration(hintText: "Senha"),
+                  obscureText: true,
+                  validator: (text) {
+                    if (text.isEmpty || text.length < 6)
+                      return "Senha inválida! A senha deve conter 6 digitos";
+                  },
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextFormField(
+                  controller: _addressController,
+                  decoration: InputDecoration(hintText: "Endereço"),
+                  validator: (text) {
+                    if (text.isEmpty) return "Endereço inválido!";
+                  },
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                SizedBox(
+                  height: 50.0,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        Map<String, dynamic> userData = {
+                          "name": _nameController.text,
+                          "email": _emailController.text,
+                          "address": _addressController.text
+                        };
+
+                        model.signUp(
+                            userData: userData,
+                            pass: _passController.text,
+                            onSuccess: _onSuccess,
+                            onFail: _onFail);
+                      }
+                    },
+                    child: Text(
+                      "CRIAR CONTA",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                    ),
+                    style: TextButton.styleFrom(
+                        primary: Colors.white,
+                        backgroundColor: Theme.of(context).primaryColor),
+                  ),
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
+
+  void _onSuccess() {}
+  void _onFail() {}
 }
