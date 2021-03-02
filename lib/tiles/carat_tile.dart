f1153b73f5368f57c08682b3a69d57d7fe431c2f
Luciano <lucianopaiva.e@gmail.com>
2021-03-02 11:01:29 -0300

    visualizando os itens no carrinho

--- lib/tiles/carat_tile.dart
+++ lib/tiles/carat_tile.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:loja/datas/cart_product.dart';
@@ -8,7 +10,68 @@ class CartTile extends StatelessWidget {
   CartTile(this.cartProduct);
   @override
   Widget build(BuildContext context) {
-    Widget _buildContent() {}
+    Widget _buildContent() {
+      Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8.0),
+            width: 120.0,
+            child: Image.network(
+              cartProduct.productData.images[0],
+              fit: BoxFit.cover,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    cartProduct.productData.title,
+                    style:
+                        TextStyle(fontWeight: FontWeight.w500, fontSize: 17.0),
+                  ),
+                  Text(
+                    "Tamanho: ${cartProduct.size}",
+                    style: TextStyle(fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                    "R\$ ${cartProduct.productData.price.toStringAsFixed(2)}",
+                    style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      IconButton(
+                          icon: Icon(Icons.remove),
+                          color: Theme.of(context).primaryColor,
+                          onPressed: cartProduct.quantity > 1 ? () {} : null),
+                      Text(cartProduct.quantity.toString()),
+                      IconButton(
+                          icon: Icon(Icons.add),
+                          color: Theme.of(context).primaryColor,
+                          onPressed: () {}),
+                      FlatButton(
+                        onPressed: () {},
+                        child: Text("Remover"),
+                        color: Colors.grey[500],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      );
+    }
+
     return Card(
       margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
       child: cartProduct.productData == null
