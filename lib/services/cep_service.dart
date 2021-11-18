e5943bc8ba62978df3b65adfd246d000d3f70ade
Luciano Paiva <lucianopaiva.e@gmail.com>
2021-11-18 12:37:25 -0300

    Transformando os dados map em um objetos

--- lib/services/cep_service.dart
+++ lib/services/cep_service.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 import 'package:dio/dio.dart';
+import 'package:loja/models/cep_model.dart';
 
 const token =
     '172c71f883c3b133b75b8c0cb57e4da0'; // Token de acesso da API do CEP aberto
 
 class CepService {
-  Future<void> getAdressFromCep(String cep) async {
+  Future<CepAddress> getAdressFromCep(String cep) async {
     final cleanCep =
         cep.replaceAll('.', '').replaceAll('-', ''); // Limpa os caracteres
     final endPoint =
@@ -16,14 +17,16 @@ class CepService {
         'Token token = $token';
 
     try {
-      final response = await dio.get<Map>(
+      final response = await dio.get<Map<String, dynamic>>(
           endPoint); // Especifica que o retorno dos dados deve ser no formato Map
 
 // Verificar se houve resposta
       if (response.data.isEmpty) {
         return Future.error('CEP Inválido');
       }
-      print(response.data); // Testando
+      final CepAddress address = CepAddress.fromMap(response.data);
+      return address;
+//      print(response.data); // Testando
     } on DioError catch (e) {
       return Future.error(
           'Erro ao Buscar CEP'); // Caso não haja internet ou outro erro de busca
